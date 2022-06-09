f12bf081313d1b273b323f00cc07668fea8b53b1
Collin Dapper <collindapper@gmail.com>
2022-06-08 23:32:04 -0700

    color adjustment

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -22,13 +22,13 @@ class LandingPage extends React.Component {
           <h1 className="fontPrimary colorSecondary display-3 text-end">Full-Stack Developer</h1>
         </div>
 
-        <div className="border-start col-11 col-xl-6 ps-4">
+        <div className="border-start col-11 col-xl-6 ps-4 updatesList">
           <h3 className="fontSecondary text-white text-start mb-0 pb-0">Latest Updates:</h3>
             <h5 className="fontPrimary color-light text-start mt-2 ms-2">June 2022 -</h5>
 
               <h5 className="fontPrimary color-light text-start ms-4"><Link className="updatesLink me-1 " to="/projects/"><i className="fas fa-code me-1"></i></Link>Published 'Currency Converter' project</h5>
 
-              <h5 className="fontPrimary color-light text-start ms-4"><a className="updatesLink me-1" href="https://www.linkedin.com/in/collin-dapper-a1b59a152/details/skills/" target="_blank"><i className="fab fa-linkedin me-1"></i></a>Endorsed for HTML, CSS, JavaScript, and Full-Stack Development</h5>
+              <h5 className="fontPrimary text-start ms-4"><a className="updatesLink me-1" href="https://www.linkedin.com/in/collin-dapper-a1b59a152/details/skills/" target="_blank"><i className="fab fa-linkedin me-1"></i></a>Endorsed for HTML, CSS, JavaScript, and Full-Stack Development</h5>
 
             <h5 className="fontPrimary color-light text-start mt-4 ms-2">May 2022 -</h5>
 
